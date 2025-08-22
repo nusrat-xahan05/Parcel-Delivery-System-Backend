c1ddc695501175b862ada6efb28423cc55f50913
Nusrat Jahan <nusrat.jahan.az@gmail.com>
2025-08-22 23:35:45 +0600

    Global Error Handler Added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { router } from "./app/routes";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 
 
 const app: Application = express();
@@ -15,6 +16,8 @@ app.get('/', (req: Request, res: Response) => {
         message: "Welcome to Parcel Delivery System Backend"
     });
 })
+
+app.use(globalErrorHandler);
 
 
 export default app;
